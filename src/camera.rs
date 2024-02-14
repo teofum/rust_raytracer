c40b55bca872c38735dc86cc5cf27494182cf17a
Teo Fumagalli <teo.fumagalli@aerolab.co>
2024-02-14 01:16:46 -0300

    feat: multisampling

--- src/camera.rs
+++ src/camera.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use crate::buffer::Buffer;
 use crate::interval::Interval;
 use crate::object::Hit;
@@ -5,6 +7,7 @@ use crate::ray::Ray;
 use crate::vec3::{Color, Point3, Vec3};
 
 const VIEWPORT_HEIGHT: f64 = 24.0 / 1000.0;
+const SAMPLES_PER_PIXEL: u32 = 10;
 
 pub struct Camera {
     image_width: usize,
@@ -16,11 +19,7 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(
-        image_width: usize,
-        aspect_ratio: f64,
-        focal_length: f64,
-    ) -> Self {
+    pub fn new(image_width: usize, aspect_ratio: f64, focal_length: f64) -> Self {
         let image_height = usize::max(1, (image_width as f64 / aspect_ratio) as usize);
 
         // Final aspect ratio, accounting for image height rounding
@@ -62,11 +61,16 @@ impl Camera {
     pub fn render(&self, world: &dyn Hit, buf: &mut Buffer) {
         for y in 0..self.image_height {
             print!("Rendering... [line {}/{}]\r", y + 1, self.image_height);
-    
+
             for x in 0..self.image_width {
-                let ray = self.get_ray(x, y);
-                let color = self.ray_color(&ray, world);
-    
+                let mut color = Vec3::origin();
+
+                for _ in 0..SAMPLES_PER_PIXEL {
+                    let ray = self.get_ray(x, y);
+                    color += self.ray_color(&ray, world);
+                }
+                color /= SAMPLES_PER_PIXEL as f64;
+
                 buf.set_pixel(x, y, color);
             }
         }
@@ -78,8 +82,9 @@ impl Camera {
         let pixel_center = self.first_pixel
             + (self.pixel_delta.0 * pixel_x as f64)
             + (self.pixel_delta.1 * pixel_y as f64);
+        let pixel_sample = pixel_center + self.pixel_sample_square();
 
-        let ray_direction = pixel_center - self.camera_center;
+        let ray_direction = pixel_sample - self.camera_center;
 
         Ray::new(self.camera_center, ray_direction)
     }
@@ -88,10 +93,18 @@ impl Camera {
         if let Some(hit) = object.test(&ray, Interval(0.0, f64::INFINITY)) {
             return (Vec3(1.0, 1.0, 1.0) + hit.normal()) * 0.5;
         }
-    
+
         let unit_dir = ray.direction().to_unit();
         let t = 0.5 * (unit_dir.y() + 1.0);
-    
+
         Vec3::lerp(Vec3(1.0, 1.0, 1.0), Vec3(0.5, 0.7, 1.0), t)
+    }
+
+    fn pixel_sample_square(&self) -> Vec3 {
+        let mut rng = rand::thread_rng();
+        let x = rng.gen_range(0.0..1.0) - 0.5;
+        let y = rng.gen_range(0.0..1.0) - 0.5;
+
+        self.pixel_delta.0 * x + self.pixel_delta.1 * y
     }
 }
